724e86426e57893265168e4963c3ee42ab4dcaf5
Samuel Kyletoft <samuel@kyletoft.se>
2022-04-25 21:18:02 +0200

    Basic upload (but then discard)

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,12 +1,26 @@
+use rocket::{
+	form::{Form, FromForm},
+	serde::{Deserialize, Serialize},
+};
+
 #[macro_use]
 extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-	"Hello, world!\n"
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, FromForm)]
+struct Upload<'r> {
+	data: &'r str,
+	user: &'r str,
+	highlighting: Option<&'r str>,
+}
+
+#[get("/upload", data = "<form>")]
+fn upload(form: Form<Upload<'_>>) -> String {
+	let data = form.into_inner();
+	dbg!(&data);
+	String::from("Upload failed\n")
 }
 
 #[launch]
 fn rocket() -> _ {
-	rocket::build().mount("/", routes![index])
+	rocket::build().mount("/", routes![upload])
 }
