50510f683eb75973ddb80a290de5e1c90b58f10a
Samuel Kyletoft <samuel@kyletoft.se>
2022-04-25 11:26:44 +0000

    Add aarch64 target

--- server/flake.nix
+++ server/flake.nix
@@ -16,7 +16,7 @@
       version = "${builtins.substring 0 8 lastModifiedDate}-${self.shortRev or "dirty"}";
 
       # System types to support.
-      supportedSystems = [ "x86_64-linux" ];
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
 
       # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
       forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
